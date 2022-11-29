802a48dce488cc59ec3b8c3d73c8cacce93ed729
Jyothsna-Siripuram <s92jyothsna@gmail.com>
2022-11-29 17:00:16 +0530

    Add full title helper"
    "

--- Gemfile
+++ Gemfile
@@ -6,10 +6,12 @@ ruby "3.1.2"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4"
 
+gem 'pg'
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
-# Use sqlite3 as the database for Active Record
+# # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
 
 # Use the Puma web server [https://github.com/puma/puma]
@@ -48,15 +50,20 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+# gem 'spork-rails'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem 'rspec-rails', '~> 6.0.0'
+  gem 'guard-rspec', require: false
 end
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+
+  gem 'guard'
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
@@ -70,8 +77,4 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
-end
-
-group :production do
-  gem 'pg'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,16 +2,30 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "Home page" do
     it "should have the h1 'Sample App'" do 
       get '/static_pages/home'
       expect(response.body).to include('<h1>Sample App</h1>')
     end
 
-    it "should have the Title 'Home'" do 
+    it "should have the base title" do
       get '/static_pages/home'
-      expect(response.body).to include('<title>Ruby on Rails Tutorial Sample App | Home</title>')
+      expect(response.body).to include('<title> Ruby on Rails Tutorial Sample App </title>')
     end
+
+    it "should not have a custom page title" do
+      get '/static_pages/home'
+      expect(response.body).to_not include('<title> | Home </title>')
+    end
+
+    # it "should have the Title 'Home'" do
+    #   get '/static_pages/home'
+    #   expect(response.body).to include('<title>' + @base_title + ' | Home</title>')
+    # end
   end
 
   describe "Help page" do
@@ -22,11 +36,11 @@ RSpec.describe "StaticPages", type: :request do
 
     it "should have the Title 'Help'" do 
       get '/static_pages/help'
-      expect(response.body).to include('<title>Ruby on Rails Tutorial Sample App | Help</title>')
+      expect(response.body).to include('<title>' + @base_title + ' | Help</title>')
     end
   end
 
-   describe "About page" do
+  describe "About page" do
     it "should have the h1 'About Us'" do 
       get '/static_pages/about'
       expect(response.body).to include('<h1>About Us</h1>')
@@ -34,7 +48,19 @@ RSpec.describe "StaticPages", type: :request do
 
     it "should have the Title 'About Us'" do 
       get '/static_pages/about'
-      expect(response.body).to include('<title>Ruby on Rails Tutorial Sample App | About Us</title>')
+      expect(response.body).to include('<title>' + @base_title + ' | About Us</title>')
+    end
+  end
+
+    describe "Contact page" do
+    it "should have the h1 'Contact'" do 
+      get '/static_pages/contact'
+      expect(response.body).to include('<h1>Contact</h1>')
+    end
+
+    it "should have the Title 'Contact'" do 
+      get '/static_pages/contact'
+      expect(response.body).to include('<title>' + @base_title + ' | Contact</title>')
     end
   end
 
